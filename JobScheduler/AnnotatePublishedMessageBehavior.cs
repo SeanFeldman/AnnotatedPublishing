ec6418e30e8218f95238ebc783da60c6c8bda19f
David Boike <david.boike@gmail.com>
2018-01-16 13:57:22 -0600

    Wrap existing subscription storage

--- JobScheduler/AnnotatePublishedMessageBehavior.cs
+++ JobScheduler/AnnotatePublishedMessageBehavior.cs
@@ -1,6 +1,5 @@
 ﻿using NServiceBus.Pipeline;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +7,7 @@ using JobScheduler.Messages;
 using NServiceBus;
 using NServiceBus.Extensibility;
 using NServiceBus.Features;
+using NServiceBus.ObjectBuilder;
 using NServiceBus.Unicast.Subscriptions;
 using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
 
@@ -41,50 +41,72 @@ namespace JobScheduler
         }
     }
 
-    class RegisterBehavior : INeedInitialization
+    public class PublishMasterJobIdEventsFeature : Feature
     {
-        public void Customize(EndpointConfiguration configuration)
+        public PublishMasterJobIdEventsFeature()
         {
-            configuration.Pipeline.Register(new ExtractJobStatusMessageIdBehavior(),
+            EnableByDefault();
+            DependsOn<MessageDrivenSubscriptions>();
+        }
+
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Pipeline.Register(new ExtractJobStatusMessageIdBehavior(),
                 "Extracts MasterJobId from JobStatusMessage and determines an annotated type to publish for it.");
 
-            configuration.Pipeline.Register(new AnnotatePublishedMessageBehavior(),
+            context.Pipeline.Register(new AnnotatePublishedMessageBehavior(),
                 "Annotates JobStatusMessage events with a type based on the MasterJobId.");
 
-            configuration.RegisterComponents(reg =>
-            {
-                reg.ConfigureComponent<ISubscriptionStorage>(() => new AlternateSubscriptionStorage(), DependencyLifecycle.SingleInstance);
-            });
+            context.RegisterStartupTask(builder => new Startup(context.Container, builder));
+        }
 
-            configuration.DisableFeature<InMemorySubscriptionPersistence>();
+        class Startup : FeatureStartupTask
+        {
+            private IConfigureComponents container;
+            private IBuilder builder;
+
+            public Startup(IConfigureComponents container, IBuilder builder)
+            {
+                this.container = container;
+                this.builder = builder;
+            }
+
+            protected override Task OnStart(IMessageSession session)
+            {
+                var realStorage = builder.Build<ISubscriptionStorage>();
+                container.RegisterSingleton<ISubscriptionStorage>(new SubscriptionStorageWrapper(realStorage));
+
+                return Task.CompletedTask;
+            }
+
+            protected override Task OnStop(IMessageSession session)
+            {
+                return Task.CompletedTask;
+            }
         }
     }
 
-    class AlternateSubscriptionStorage : ISubscriptionStorage
+    class SubscriptionStorageWrapper : ISubscriptionStorage
     {
+        private ISubscriptionStorage realStorage;
+
+        public SubscriptionStorageWrapper(ISubscriptionStorage realStorage)
+        {
+            this.realStorage = realStorage;
+        }
+
         public Task Subscribe(Subscriber subscriber, MessageType messageType, ContextBag context)
         {
-            // Equivalent SQL: https://github.com/Particular/NServiceBus.Persistence.Sql/blob/develop/src/SqlPersistence/Subscription/SqlDialect_MsSqlServer.cs#L20-L40
-            var dict = storage.GetOrAdd(messageType, type => new ConcurrentDictionary<string, Subscriber>(StringComparer.OrdinalIgnoreCase));
-
-            dict.AddOrUpdate(subscriber.TransportAddress, _ => subscriber, (_, __) => subscriber);
-            return Task.CompletedTask;
+            return realStorage.Subscribe(subscriber, messageType, context);
         }
 
         public Task Unsubscribe(Subscriber subscriber, MessageType messageType, ContextBag context)
         {
-            // Equivalent SQL: https://github.com/Particular/NServiceBus.Persistence.Sql/blob/develop/src/SqlPersistence/Subscription/SqlDialect_MsSqlServer.cs#L46-L50
-            if (storage.TryGetValue(messageType, out var dict))
-            {
-                dict.TryRemove(subscriber.TransportAddress, out var _);
-            }
-            return Task.CompletedTask;
+            return realStorage.Unsubscribe(subscriber, messageType, context);
         }
 
         public Task<IEnumerable<Subscriber>> GetSubscriberAddressesForMessage(IEnumerable<MessageType> messageTypes, ContextBag context)
         {
-            var result = new HashSet<Subscriber>();
-
             if (context is IOutgoingPublishContext publishContext)
             {
                 if (publishContext.Message.Instance is JobStatusMessage jobStatusMsg)
@@ -92,22 +114,11 @@ namespace JobScheduler
                     var annotatedType = $"JobScheduler.AnnotatedMessages.MasterJobId{jobStatusMsg.MasterJobId}Happened, JobScheduler.AnnotatedMessages, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
                     var newMessageType = new MessageType(annotatedType);
 
-                    messageTypes = messageTypes.Union(new [] { newMessageType });
+                    messageTypes = messageTypes.Union(new[] { newMessageType });
                 }
             }
 
-            // Equivalent SQL: https://github.com/Particular/NServiceBus.Persistence.Sql/blob/develop/src/SqlPersistence/Subscription/SqlDialect_MsSqlServer.cs#L55-L73
-
-            foreach (var m in messageTypes)
-            {
-                if (storage.TryGetValue(m, out var list))
-                {
-                    result.UnionWith(list.Values);
-                }
-            }
-            return Task.FromResult((IEnumerable<Subscriber>)result);
+            return realStorage.GetSubscriberAddressesForMessage(messageTypes, context);
         }
-
-        ConcurrentDictionary<MessageType, ConcurrentDictionary<string, Subscriber>> storage = new ConcurrentDictionary<MessageType, ConcurrentDictionary<string, Subscriber>>();
     }
 }
